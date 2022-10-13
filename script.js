4459ab2030b844d5719d59ed8b12f3fa050896e0
ralixt <ragee.r@outlook.com>
2022-10-13 06:42:48 +0200

    ajout fonctions nav

--- script.js
+++ script.js
@@ -7,6 +7,28 @@ menuHamburger.addEventListener('click',()=>{
 })
 
 
+const liste = document.querySelectorAll(".active1")
+for ( i = 0; i < liste.length; i++) {
+    liste[i].addEventListener('click', function () {
+        this.classList.toggle('cliqueListe')
+    })
+}
+
+
+// const liste = document.getElementBy
+// liste.addEventListener('click', ()=>{
+//     nav.classList.toggle('cliqueListe')
+// })
+
+// const elements = document.getElementsByClassName(".active1")
+// for(var i = 0; i < elements.length; i++){
+//     // Same like above...
+//     menuHamburger.classList.toggle('cliqueListe')
+// }
+
+
+
+
 /*Accordion*/ 
 const accordion = document.getElementsByClassName("contentBox");
 for (i = 0; i< accordion.length; i++ ){
@@ -14,4 +36,6 @@ for (i = 0; i< accordion.length; i++ ){
         this.classList.toggle('act')
     })
 }
+
+
 
